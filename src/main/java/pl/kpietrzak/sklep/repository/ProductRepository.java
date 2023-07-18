b7b00756cc8ef4bb0a8564d050b141030966e13e
kPietrzak <kPietrzak555@gmail.com>
2023-07-18 17:43:23 +0200

    Obsługa Kategorii w produktach

--- src/main/java/pl/kpietrzak/sklep/repository/ProductRepository.java
+++ src/main/java/pl/kpietrzak/sklep/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package pl.kpietrzak.sklep.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.kpietrzak.sklep.model.Category;
 import pl.kpietrzak.sklep.model.Product;
 
+import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(Category category);
 }
+
+
 
